41aa124ea6fecb9bb2d206d7456516e0b1ecb05c
Boris <borisonekenobi@gmail.com>
2021-01-16 00:53:14 -0500

    Added TODO statement to crypt.js

--- crypt.js
+++ crypt.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt');
 
 function encrypt(password) {
     bcrypt.hash(password, 5, function (err, encryptedPassword) {
+        //TODO: save encryptedPassword to mongodb
         console.log(password + ' encrypted to ' + encryptedPassword);
     });
 }

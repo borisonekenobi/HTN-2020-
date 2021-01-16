66659e5e0c046b612a9c2a7cad6bf2e049af68f7
Boris <borisonekenobi@gmail.com>
2021-01-16 07:49:11 -0500

    Changes:
    added stub checkPassword function to crypt.js
    added signup.js with stub function
    added signup.html referencing stub function
    added signup.js to main.html as script

--- crypt.js
+++ crypt.js
@@ -3,10 +3,13 @@ const bcrypt = require('bcrypt');
 function encrypt(password) {
     bcrypt.hash(password, 5, function (err, encryptedPassword) {
         //TODO: save encryptedPassword to mongodb
-        console.log(password + ' encrypted to ' + encryptedPassword);
     });
 }
 
-encrypt('password');
+function checkPassword(password) {
+    bcrypt.hash(password, 5, function (err, encryptedPassword) {
+        //TODO: check if mongodb password and encrypted password match
+    });
+}
 
 module.exports = {encrypt}

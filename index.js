24b5cdf9883c87b33a843d091f2b1a197ee0e9e7
Boris <borisonekenobi@gmail.com>
2021-01-16 01:08:08 -0500

    Fixed scrolling function

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the header
-let header = document.getElementById("myHeader");
-
-// Get the offset position of the navbar
-let sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
-    }
-}
-
 const port = 8000;
 const hostname = 'localhost'
 
 const http = require('http');
 const fs = require('fs');
 
-fs.readFile('index.html', (err, html) => {
+fs.readFile('main.html', (err, html) => {
     if (err) {
         throw err;
     }

4ffd1beabe074cd9d00d38de3b96d35cb7caf870
Boris <borisonekenobi@gmail.com>
2021-01-17 02:11:32 -0500

    encrypt now returns the encrypted password instead of storing it

--- crypt.js
+++ crypt.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt');
 
 function encrypt(password) {
     bcrypt.hash(password, 5, function (err, encryptedPassword) {
-        //TODO: save encryptedPassword to mongodb
+        return encryptedPassword;
     });
 }
 

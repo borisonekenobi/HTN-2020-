20e99a7864a47a9dc3f3e0a582274510a977ab83
Boris <borisonekenobi@gmail.com>
2021-01-17 02:13:08 -0500

    Made search.js and search.html instead of matchingSystem.js

--- css/matchingSystem.js
+++
@@ -1,7 +0,0 @@
-function findMentors(mentee) {
-    const tags = mentee.tags;
-
-    //search database and return those with matching tags
-}
-
-module.exports = {findMentors};
---
+++ search.js
@@ -0,0 +1,25 @@
+function search(query) {
+    let mentors = [];
+    const users = require('./userData.json');
+
+    for (let i = 0; i < Object.keys(users).length; i++) {
+        let user = users[i.toString()];
+        if (user.Role === 'mentor') {
+            for (let j = 0; j < user.Fields.length; j++) {
+                if (query === user.Fields[j]) {
+                    mentors.push(user);
+                }
+            }
+        }
+    }
+    //return 'test';
+
+    let arrayToString = JSON.stringify(Object.assign({}, mentors));  // convert array to string
+    return JSON.parse(arrayToString);
+    /**
+     * ^ yes this is copied from the internet ^
+     * don't ask me what it does cuz idk
+     **/
+}
+
+module.exports = {search}
